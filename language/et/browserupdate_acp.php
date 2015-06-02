3fee419ab0f97854d5899d5528394b86a2d0d817
HoLyCoWzOrZ <me@holycowzorz.com>
2015-06-02 00:50:20 -0700

    Updated ET formatting
    Updated the formatting for the ET translation files. Also added a
    missing php starting tag.

--- language/et/browserupdate_acp.php
+++ language/et/browserupdate_acp.php
@@ -7,6 +7,7 @@
  * @license GNU General Public License, version 2 (GPL-2.0)
  *
  */
+
 /**
  * DO NOT CHANGE
  */
@@ -14,18 +15,22 @@ if (!defined('IN_PHPBB'))
 {
 	exit;
 }
+
 if (empty($lang) || !is_array($lang))
 {
 	$lang = array();
 }
+
 $lang = array_merge($lang, array(
 	'BROWSERUPDATE_SETTINGS'				=> 'Uuenda veebilehitsejat seaded',
 	'BROWSERUPDATE_SETTINGS_EXPLAIN'		=> 'Siin saad muuta laienduse "Uuenda veebilehitsejat" seadeid',
 	'BROWSERUPDATE_SETTINGS_SAVED'			=> 'Seaded on edukalt salvestatud.',
+
 	'BROWSERUPDATE_REMINDER'					=> 'Kui tihedasti peaks seda teadet kordama',
 	'BROWSERUPDATE_REMINDER_EXPLAIN'			=> 'Antud teadet näidatakse ühe korra iga antud tunni tagant. Seadista väärtuseks 0 kui soovid, et teadet näidatakse alati igal ajal.',
 	'BROWSERUPDATE_REMINDER_CLOSED'				=> 'Kui kasutaja sulgeb teate, siis millal teade peaks uuesti esile tulema',
 	'BROWSERUPDATE_REMINDER_CLOSED_EXPLAIN'		=> 'Kasutajad saavad antud teadet ignoreerida sulgedes selle. See seade toob teate taas esile peale ära määratud tundi.',
 	'BROWSERUPDATE_NEW_WINDOW'					=> 'Ava link uues aknas / vahelehes',
+
 	'BROWSERUPDATE_SETTING_HOURS'		=> 'Tundi',
 ));
